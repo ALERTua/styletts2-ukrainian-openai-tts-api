eaee60b1648c653f3dc9bd8979030cf845f4e40d
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2025-01-21 20:08:49 +0200

    - softlinks

--- script/deploy_remote.cmd
+++ script/deploy_remote.cmd
@@ -1,1 +1,6 @@
-C:/ALERT/Hardlinks/deploy_remote.cmd
+
+@echo off
+if not defined DOCKER_HOST set DOCKER_HOST=tcp://docker:2375
+set DOCKER_REMOTE=1
+call %~dp0deploy.cmd %*
+where nircmd >nul 2>nul && nircmd beep 500 500
