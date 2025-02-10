a5fcbb0641812116e8b6c26e96bb804dbcff5f4c
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2025-02-10 21:37:37 +0200

    ! manual build script tweaks

--- script/manual_build.cmd
+++ script/manual_build.cmd
@@ -1,12 +1,27 @@
 
 @echo off
+setlocal enabledelayedexpansion enableextensions
+
 if not defined DOCKER_HOSTNAME set DOCKER_HOSTNAME=docker
 if not defined DOCKER_HOST set DOCKER_HOST=tcp://%DOCKER_HOSTNAME%:2375
 if not defined STAGE set STAGE=production
 if not defined CONTAINER_NAME set CONTAINER_NAME=test
 if not defined DOCKERFILE set DOCKERFILE=Dockerfile
+set TAG_BASE=ghcr.io/alertua/styletts2-ukrainian-openai-tts-api
 
-set TAG=ghcr.io/alertua/styletts2-ukrainian-openai-tts-api:latest
+choice /C YN /m "latest?"
+if "%errorlevel%"=="1" (
+    set TAG=%TAG_BASE%:latest
+) else (
+    choice /C YN /m "all?"
+    if "!errorlevel!"=="1" (
+        set TAG=%TAG_BASE%:all
+    ) else (
+        echo no option given
+        exit /b 1
+    )
+
+)
 
 choice /C YN /d N /T 15 /m "Build %TAG%?"
 if "%errorlevel%"=="1" (
