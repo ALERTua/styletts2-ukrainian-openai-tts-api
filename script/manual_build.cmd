4e092ea41d5a48d65cfb938f42b3f1d8243a58db
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2025-02-10 21:31:32 +0200

    ! manual build script tweaks

--- script/manual_build.cmd
+++ script/manual_build.cmd
@@ -8,7 +8,12 @@ if not defined DOCKERFILE set DOCKERFILE=Dockerfile
 
 set TAG=ghcr.io/alertua/styletts2-ukrainian-openai-tts-api:latest
 
-docker build -f %DOCKERFILE% --target %STAGE% -t %TAG% .
+choice /C YN /d N /T 15 /m "Build %TAG%?"
+if "%errorlevel%"=="1" (
+    docker build -f %DOCKERFILE% --target %STAGE% -t %TAG% .
+)
 
-echo PUSH IT WITH:
-echo docker push %TAG%
+choice /C YN /m "Push %TAG%?"
+if "%errorlevel%"=="1" (
+    docker push %TAG%
+)
