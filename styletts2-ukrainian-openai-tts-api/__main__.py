1384996ebdf1ad9e607b02f4bffdb1478f283798
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2025-02-10 10:38:17 +0200

    pep8

--- styletts2-ukrainian-openai-tts-api/__main__.py
+++ styletts2-ukrainian-openai-tts-api/__main__.py
@@ -152,9 +152,11 @@ async def synthesize(body: CreateSpeechRequestBody) -> StreamingResponse:
 
     LOG.info(f"input: {input}, {voice=}, {speed=}, {response_format=}, {sample_rate=}")
     if model == 'multi':
-      wavs, phonemes = inference(model=model, text=input_, voice_audio=voice_path, speed=speed, alpha=0, beta=0, diffusion_steps=20, embedding_scale=1.0)
+        wavs, phonemes = inference(model=model, text=input_, voice_audio=voice_path, speed=speed, alpha=0, beta=0,
+                                   diffusion_steps=20, embedding_scale=1.0)
     else:
-      wavs, phonemes = inference(model=model, text=input_, voice_audio=voice_path, speed=speed, alpha=1, beta=0, diffusion_steps=4, embedding_scale=1.0)
+        wavs, phonemes = inference(model=model, text=input_, voice_audio=voice_path, speed=speed, alpha=1, beta=0,
+                                   diffusion_steps=4, embedding_scale=1.0)
     # noinspection PyTypeChecker
     audio = gr.Audio((sample_rate, wavs), label="Audio:", autoplay=False, streaming=False, type="numpy",
                      format=response_format)
